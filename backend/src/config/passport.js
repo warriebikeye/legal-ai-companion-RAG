45c4a1dbaa903714efec5cd2a49799582960e43e
Warri Ebikeye <webikeye@unionbankng.com>
2026-01-16 09:44:16 +0100

    fixing google cb url - 1/16/2026

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -4,12 +4,18 @@ import pkg from "passport-google-oauth20";
 
 const { Strategy: GoogleStrategy } = pkg;
 
+// Set the callback URL based on the environment
+const callbackURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.GOOGLE_CALLBACK_URL_PROD
+    : process.env.GOOGLE_CALLBACK_URL_TEST;
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: callbackURL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
