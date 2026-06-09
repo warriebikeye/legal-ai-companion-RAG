90d061086934977d5fd3b1b277a88ec38a20688e
Warri Ebikeye Cyprian <153061110+warriebikeye@users.noreply.github.com>
2026-06-09 12:44:09 +0100

    VERIFICATION PORT ISSUE

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4,             // force IPv4
+  port: 587,
+  secure: false,   // false for port 587 (STARTTLS)
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
